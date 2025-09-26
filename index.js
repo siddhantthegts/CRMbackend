dad24c3f490b62347593443f339c923a7e05affd
siddhantthegts <siddhantthegts@gmail.com>
2025-09-27 03:37:52 +0530

    cors fixed and updated

--- index.js
+++ index.js
@@ -18,17 +18,11 @@ const cfg = {
 const app = express();
 
 app.use(compression());
-// var whitelist = ['http://localhost:3000']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-// app.use(cors(corsOptions));
+app.use(cors({
+  origin: ["http://localhost:3000", "https://your-frontend-domain.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
