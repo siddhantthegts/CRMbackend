1c566b93b13656051495d1b0dee133581bcc9656
siddhantthegts <siddhantthegts@gmail.com>
2025-09-27 03:29:39 +0530

    cors allowed for onlyu localhost

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ const cfg = {
 const app = express();
 
 app.use(compression());
-app.use(cors());
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
